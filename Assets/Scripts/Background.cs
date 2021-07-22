00db4f6151dfab0f0865ef242bbe261089236368
yoVeronn <ban.veronike@gmail.com>
2021-07-22 18:09:05 +0800

    Update 4.3
    - Parallax background added for all 3 levels (attached to camera)

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Background : MonoBehaviour
 {
+    private float length, startPos;
+    public GameObject referencePoint;
+    public float parallaxEffect;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position.x;
+        length = (GetComponent<SpriteRenderer>().bounds.size.x);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float temp = (referencePoint.transform.position.x * (1 - parallaxEffect));
+        float dist = (referencePoint.transform.position.x * parallaxEffect);
+        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+
+        if (temp > (startPos + length))
+        {
+            startPos += length;
+        }
+        else if (temp < (startPos - length))
+        {
+            startPos -= length;
+        }
     }
 }
