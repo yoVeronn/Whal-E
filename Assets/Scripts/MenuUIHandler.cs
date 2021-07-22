e4c43a9cd1eab30f4f6b4ba178ff9a964718263c
yoVeronn <ban.veronike@gmail.com>
2021-07-22 10:32:55 +0800

    Update 4.1
    - Pause panel fully programmed (restart and back to main buttons)
    - Info menu fully programmed

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -8,6 +8,9 @@ using UnityEditor;
 
 public class MenuUIHandler : MonoBehaviour
 {
+    public GameObject pauseScreen;
+    public bool paused = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,21 +19,28 @@ public class MenuUIHandler : MonoBehaviour
 
     public void StartNew()
     {
+        paused = false;
+        Time.timeScale = 1;
         SceneManager.LoadScene(1);
     }
 
     public void LoadLevel2()
     {
+        paused = false;
+        Time.timeScale = 1;
         SceneManager.LoadScene(2);
     }
 
     public void LoadLevel3()
     {
+        paused = false;
+        Time.timeScale = 1;
         SceneManager.LoadScene(3);
     }
 
     public void LoadMenu()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(0);
     }
 
@@ -43,9 +53,28 @@ public class MenuUIHandler : MonoBehaviour
 #endif
     }
 
+    void ChangePause()
+    {
+        if (!paused)
+        {
+            paused = true;
+            pauseScreen.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            paused = false;
+            pauseScreen.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ChangePause();
+        }
     }
 }
